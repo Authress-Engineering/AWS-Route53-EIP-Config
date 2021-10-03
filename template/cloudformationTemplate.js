fdb24791ec1b003eeb445ec56c6f30068466e19b
Warren Parad <wparad@gmail.com>
2021-10-03 19:22:16 +0200

    Add config rule and executing lambda function.

---
+++ template/cloudformationTemplate.js
@@ -0,0 +1,108 @@
+module.exports = {
+  getTemplate(lambdaFunctionString) {
+    return {
+      AWSTemplateFormatVersion: '2010-09-09',
+      Transform: 'AWS::Serverless-2016-10-31',
+      Description: 'Route53 Config rule for EIP',
+      Parameters: {},
+
+      Resources: {
+        ConfigRule: {
+          Type: 'AWS::Config::ConfigRule',
+          Properties: {
+            ConfigRuleName: { 'Fn::Sub': '${AWS::StackName}-${AWS::Region}' },
+            Description: 'Route53 Config rule for EIP',
+            Source: {
+              Owner: 'CUSTOM_LAMBDA',
+              SourceDetails: [{
+                EventSource: 'aws.config',
+                MaximumExecutionFrequency: 'TwentyFour_Hours',
+                MessageType: 'ScheduledNotification'
+              }],
+              SourceIdentifier: { 'Fn::Sub': '${LambdaFunction.Arn}' }
+            }
+          },
+          DependsOn: 'PermissionForConfigToInvokeLambda'
+        },
+
+        PermissionForConfigToInvokeLambda: {
+          Type: 'AWS::Lambda::Permission',
+          Properties: {
+            FunctionName: { 'Fn::Sub': '${LambdaFunction.Arn}' },
+            Action: 'lambda:InvokeFunction',
+            Principal: 'config.amazonaws.com',
+            SourceAccount: { Ref: 'AWS::AccountId' }
+          }
+        },
+
+        LambdaFunction: {
+          Type: 'AWS::Lambda::Function',
+          Properties: {
+            Description: 'Triggered by the Config Rule',
+            FunctionName: { 'Fn::Sub': '${AWS::StackName}-${AWS::Region}' },
+            Handler: 'index.handler',
+            Role: { 'Fn::GetAtt': ['AWSLambdaExecutionRole', 'Arn'] },
+            Timeout: 900,
+            Runtime: 'nodejs12.x',
+            Code: {
+              ZipFile: lambdaFunctionString
+            }
+          }
+        },
+
+        CloudWatchLambdaLogGroup: {
+          Type: 'AWS::Logs::LogGroup',
+          Properties: {
+            LogGroupName: { 'Fn::Sub': '/aws/lambda/${AWS::StackName}-${AWS::Region}' },
+            RetentionInDays: 365
+          }
+        },
+
+        AWSLambdaExecutionRole: {
+          Type: 'AWS::IAM::Role',
+          Properties: {
+            AssumeRolePolicyDocument: {
+              Statement: [
+                {
+                  Action: ['sts:AssumeRole'],
+                  Effect: 'Allow',
+                  Principal: {
+                    Service: ['lambda.amazonaws.com']
+                  }
+                }
+              ],
+              Version: '2012-10-17'
+            },
+            Path: '/',
+            ManagedPolicyArns: [
+              'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
+            ],
+            Policies: [
+              {
+                PolicyDocument: {
+                  Statement: [
+                    {
+                      Action: ['route53:ListHostedZones', 'route53:ListResourceRecordSets', 'ec2:describeRegions', 'ec2:DescribeAddresses', 'config:PutEvaluations'],
+                      Effect: 'Allow',
+                      Resource: '*'
+                    }
+                  ],
+                  Version: '2012-10-17'
+                },
+                PolicyName: { 'Fn::Sub': '${AWS::StackName}-${AWS::Region}-AWSLambda-Access' }
+              }
+            ],
+            RoleName: { 'Fn::Sub': '${AWS::StackName}-${AWS::Region}' }
+          }
+        }
+      },
+
+      Outputs: {
+        ConfigRuleId: {
+          Description: 'Config Rule ID',
+          Value: { 'Fn::Sub': '${ConfigRule.ConfigRuleId}' }
+        }
+      }
+    };
+  }
+};
---
+++ template/configRule.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-console */
+// http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-lambda-function-code.html
+const { Route53, EC2, ConfigService } = require('aws-sdk');
+
+const COMPLIANCE_STATES = {
+  COMPLIANT: 'COMPLIANT',
+  NON_COMPLIANT: 'NON_COMPLIANT',
+  NOT_APPLICABLE: 'NOT_APPLICABLE'
+};
+
+exports.handler = async function(event) {
+//   const invokingEvent = JSON.parse(event.invokingEvent);
+//   const ruleParameters = JSON.parse(event.ruleParameters);
+  const route53 = new Route53();
+  let hostedZoneIds;
+  try {
+    console.log('Looking up hosted zones:');
+    hostedZoneIds = (await route53.listHostedZones({ }).promise()).HostedZones.map(hz => hz.Id);
+    console.log('   :', hostedZoneIds);
+  } catch (error) {
+    console.log('  Failed to retrieve hosted zones', error);
+    throw error;
+  }
+
+  const addresses = [];
+  const regions = await new EC2().describeRegions().promise().then(data => data.Regions.map(r => r.RegionName));
+  await Promise.all(regions.map(async region => {
+    const regionalEc2Client = new EC2({ region });
+    addresses.push(...(await regionalEc2Client.describeAddresses().promise()).Addresses.map(a => a.PublicIp));
+  }));
+  
+  const evaluationResults = [];
+  await Promise.all(hostedZoneIds.map(async hostedZoneId => {
+    console.log('Fetching records for zone: ', hostedZoneId);
+    const recordAddressMap = {};
+    const params = { HostedZoneId: hostedZoneId };
+    try {
+      do {
+        const response = await route53.listResourceRecordSets(params).promise();
+        params.StartRecordIdentifier = response.NextRecordIdentifier;
+        response.ResourceRecordSets
+        .filter(t => !t.AliasTarget && t.Type === 'A')
+        .map(r => ({ hostedZoneId, name: r.Name, type: r.Type, originalRecord: r }))
+        .forEach(r => {
+          r.originalRecord.ResourceRecords.map(rr => rr.Value).forEach(address => {
+            if (!recordAddressMap[address]) {
+              recordAddressMap[address] = [];
+            }
+            recordAddressMap[address].push(r);
+          });
+        });
+      } while (params.StartRecordIdentifier);
+    } catch (error) {
+      console.error(`Failed to get records for zone: ${hostedZoneId}:`, error);
+    }
+
+    if (!Object.keys(recordAddressMap).length) {
+      return;
+    }
+
+    addresses.forEach(a => {
+      delete recordAddressMap[a];
+    });
+
+    const recordMap = {};
+    Object.keys(recordAddressMap).forEach(ipAddress => {
+      recordAddressMap[ipAddress].forEach(record => {
+        if (!recordMap[record.name]) {
+          recordMap[record.name] = { name: record.name, type: record.type, hostedZoneId: hostedZoneId.replace('/hostedzone/', ''), ipAddresses: {} };
+        }
+        recordMap[record.name].ipAddresses[ipAddress] = true;
+      });
+    });
+    console.log('Records with non-existent Ip Addresses:', Object.keys(recordMap));
+
+    evaluationResults.push(...Object.values(recordMap).map(record => ({
+      Annotation: `IPv4 ${Object.keys(record.ipAddresses).join(', ')}`,
+      ComplianceResourceType: 'AWS::::Account',
+      ComplianceResourceId: `arn:aws:route53::${event.accountId}:${hostedZoneId}:${record.name}:${record.type}`,
+      ComplianceType: COMPLIANCE_STATES.NON_COMPLIANT,
+      OrderingTimestamp: new Date()
+    })));
+  }));
+
+  const configService = new ConfigService();
+  await configService.putEvaluations({ Evaluations: evaluationResults, ResultToken: event.resultToken }).promise();
+};
