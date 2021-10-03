8cb18ef3d77e765783ae5ead86003fbe33127969
Warren Parad <wparad@gmail.com>
2021-10-03 16:48:42 +0200

    initial setup.

---
+++ README.md
@@ -0,0 +1,22 @@
+# AWS S3 Explorer
+
+<p align="center">
+    <a href="./LICENSE" alt="apache 2.0 license">
+      <img src="https://img.shields.io/badge/license-Apache%202.0-blue.svg">
+    </a>
+    <a href="https://eu-west-1.console.aws.amazon.com/lambda/home?region=eu-west-1#/create/app?applicationId=arn:aws:serverlessrepo:eu-west-1:922723803004:applications/S3-Explorer" alt="AWS Serverless Application">
+        <img src="https://img.shields.io/badge/AWS%20Serverless%20Application-S3%20Explorer-blue">
+    </a>
+</p>
+
+
+Creates an AWS Config rule to detect route53 records pointing at ip addresses that are no longer in use. Deploy using the AWS Service application repository.
+
+Route 53 EIP config rule in [AWS Service application repository]()
+
+## Contribution
+
+### Development
+This project uses Vue 3, and as this is much different from Vue 2, recommend reading is available:
+* [General Updates](https://v3.vuejs.org/guide/computed.html)
+* [Script Setup tags](https://v3.vuejs.org/api/sfc-script-setup.html)
---
+++ make.js
@@ -0,0 +1,117 @@
+/* eslint-disable no-console */
+
+const axios = require('axios');
+const aws = require('aws-sdk');
+const commander = require('commander');
+const fs = require('fs-extra');
+// const githubActionsRunner = require('ci-build-tools')(process.env.GITHUB_TOKEN);
+
+aws.config.update({ region: 'eu-west-1' });
+
+async function setupAWS() {
+  if (!process.env.GITHUB_TOKEN || process.env.AWS_ACCESS_KEY_ID) { return; }
+  try {
+    const tokenResponse = await axios.post('https://login.rhosys.ch/api/authentication/github/tokens',
+      { client_id: 'fZqy3XNgvtAcKonjfAu9WF', grant_type: 'client_credentials' },
+      { headers: { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` } }
+    );
+
+    aws.config.credentials = new aws.WebIdentityCredentials({
+      WebIdentityToken: tokenResponse.data.access_token,
+      RoleArn: `arn:aws:iam::${process.env.AWS_ACCOUNT_ID}:role/GitHubRunnerAssumedRole`,
+      RoleSessionName: `GitHubRunner-${process.env.GITHUB_REPOSITORY}-${process.env.GITHUB_RUN_NUMBER}`,
+      DurationSeconds: 3600
+    });
+  } catch (error) {
+    const loggedError = error.response && {
+      data: error.response.data,
+      status: error.response.status,
+      headers: error.response.headers,
+      config: error.config || error.response.config
+    } || error;
+    console.log('Failed to exchange token for AWS credentials:', JSON.stringify(loggedError, null, 2));
+    process.exit(1);
+  }
+
+  try {
+    const stsResult = await new aws.STS().getCallerIdentity().promise();
+    console.log('Configured AWS Credentials', stsResult);
+  } catch (error) {
+    console.log('Failed to setup credentials', error);
+    process.exit(1);
+  }
+}
+
+function getVersion() {
+  let release_version = '0.0';
+  const pull_request = '';
+  const branch = process.env.GITHUB_REF;
+  const build_number = `${process.env.GITHUB_RUN_NUMBER}`;
+
+  // Builds of pull requests
+  if (pull_request && !pull_request.match(/false/i)) {
+    release_version = `0.${pull_request}`;
+  } else if (!branch || !branch.match(/^(refs\/heads\/)?release[/-]/i)) {
+    // Builds of branches that aren't master or release
+    release_version = '0.0';
+  } else {
+    // Builds of release branches (or locally or on server)
+    release_version = branch.match(/^(?:refs\/heads\/)?release[/-](\d+(?:\.\d+){0,3})$/i)[1];
+  }
+  return `${release_version}.${(build_number || '0')}.0.0.0.0`.split('.').slice(0, 3).join('.');
+}
+const version = getVersion();
+commander.version(version);
+
+const packageMetadata = require('./package.json');
+packageMetadata.version = version;
+
+/**
+  * Build
+  */
+commander
+  .command('setup')
+  .description('Setup require build files for npm package.')
+  .action(async () => {
+    await fs.writeJson('./package.json', packageMetadata, { spaces: 2 });
+
+    console.log('Building package %s (%s)', packageMetadata.name, version);
+    console.log('');
+  });
+
+/**
+  * After Build
+  */
+commander
+  .command('after_build')
+  .description('Publishes git tags and reports failures.')
+  .action(async () => {
+    console.log('After build package %s (%s)', packageMetadata.name, version);
+    console.log('');
+    await setupAWS();
+
+    try {
+      const serverlessApplicationRepository = new aws.ServerlessApplicationRepository();
+      const template = require('./template/cloudformationTemplate.json');
+      const params = {
+        ApplicationId: `arn:aws:serverlessrepo:${aws.config.region}:${process.env.AWS_ACCOUNT_ID}:applications/Elastic-IP-Cleaner-Config`,
+        SemanticVersion: version,
+        // SourceCodeArchiveUrl: ``,
+        SourceCodeUrl: `https://github.com/Rhosys/AWS-Route53-EIP-Config/releases/tag/${version}`,
+        TemplateBody: typeof template === 'object' ? JSON.stringify(template) : template
+        // TemplateUrl: `https://s3.amazonaws.com/${apiOptions.deploymentBucket}/cloudFormationTemplate.json`
+      };
+      await serverlessApplicationRepository.createApplicationVersion(params).promise();
+    } catch (error) {
+      console.log('Failed to push new application version', error);
+      process.exit(1);
+    }
+  });
+
+commander.on('*', () => {
+  if (commander.args.join(' ') === 'tests/**/*.js') { return; }
+  console.log(`Unknown Command: ${commander.args.join(' ')}`);
+  commander.help();
+  process.exit(0);
+});
+commander.parse(process.argv[2] ? process.argv : process.argv.concat(['build']));
